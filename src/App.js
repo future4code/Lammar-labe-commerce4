568ba6c10aeff4fbf639aa6478473b9d6cc8f5bd
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-07-17 11:14:30 -0300

    Corrige App.js após merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import './App.css';
 import React, {useState} from 'react';
 import { ShoppingCart } from './Components/ShoppingCart'
 import { mockDb } from './MockUpDados'
-import { ProductCard } from './Components/ProductCard'
+import { ProductCards } from './Components/ProductCards'
 
 function App() {
   const [productDb, setProductDb] = useState(mockDb)
