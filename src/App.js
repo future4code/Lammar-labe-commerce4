0308c9d280617a49d1f1c7d895bb7841f074dba0
Lucas <luucasferreiira21@gmail.com>
2022-07-19 19:15:55 -0300

    adicionar footer

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
 import './App.css';
 import React, {useState} from 'react';
-import { ShoppingCart } from './Components/ShoppingCart'
 import { mockDb } from './MockUpDados'
 import { ProductCards } from './Components/ProductCards'
-import {LeftBar} from './Components/LeftBar'
+import { CopyRight } from './Components/CopyRight';
 
 
 function App() {
   const [productDb, setProductDb] = useState(mockDb)
   const [cartList, setCartList] = useState([])
-  const [busca, setBusca] = useState("")
-  const [minPreco, setMinPreco] =useState(-Infinity)
-  const [maxPreco, setMaxPreco] =useState(Infinity)
-  const addProduct = () => {
-  }
+ 
   return (
     <>
-      <LeftBar
-      minPreco={minPreco}
-      maxPreco={maxPreco}
-      busca={busca}
-      setMinPreco={setMinPreco}
-      setMaxPreco={setMaxPreco}
-      setBusca={setBusca}/>
-      <>
-      {productDb.filter((produto)=>{
-        return produto.price >= minPreco || minPreco ===""
-      })
-      .filter((produto)=>{
-        return produto.price <= maxPreco || maxPreco ===""
-      })
-      .filter((produto)=>{
-        return produto.name.includes(busca)
-      })
-
-      .map(mockDb => {
-        return <>key={mockDb.id} mockDb={mockDb}</> 
-      })}</>
-      {/* <ShoppingCart cartListProp={cartList}/> */}
-      {/* <ProductCards/> */}
+    
+      <ProductCards dbListProp={productDb}/>
+      
+      <CopyRight/>
     </>
   );
 }
---
+++ src/Components/CopyRight.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer } from "../Style";
+
+
+export function CopyRight(){
+
+    return(
+        <Footer>
+
+            <p>Copyright © 2022 LabCommerce Rafael Castillho, Mauricio Gonçalves e Lucas Ferreira. All rights reserved.</p>
+        
+        </Footer>
+    )
+
+}
