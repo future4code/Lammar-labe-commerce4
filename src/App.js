0f6a5bf74646d6a3575aabce96874fd1bf1706b2
Lucas <luucasferreiira21@gmail.com>
2022-07-21 21:58:38 -0300

    atualizar app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React, {useState} from 'react';
 import { mockDb } from './MockUpDados'
 import { ProductCards } from './Components/ProductCards'
 import { CopyRight } from './Components/CopyRight';
+import { GrowingPage } from './Components/Growing';
 
 
 function App() {
   const [productDb, setProductDb] = useState(mockDb)
   const [cartList, setCartList] = useState([])
+  
  
   return (
     <>
     
       <ProductCards dbListProp={productDb}/>
-      
+             
       <CopyRight/>
     </>
   );
